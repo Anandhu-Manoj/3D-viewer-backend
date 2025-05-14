df6d865ba9540878d3ba8be93b9c2ed165529f62
Anandhu-Manoj <manojanandhu74@gmail.com>
2025-05-14 18:47:09 +0530

    render finished

--- app.js
+++ app.js
@@ -4,7 +4,21 @@ const modelRoutes = require('./Routes/route');
 const path=require('path')
 
 const app = express();
-app.use(cors({origin: 'https://glb-viewer-ffn8c11g8-anandhu-manojs-projects-2d7d8f6e.vercel.app'}));
+const allowedOrigins = [
+  'https://glb-viewer-ffn8c11g8-anandhu-manojs-projects-2d7d8f6e.vercel.app',
+  'https://glb-viewer-673p2p6ds-anandhu-manojs-projects-2d7d8f6e.vercel.app'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
+
 app.use(express.json());
 app.use('/models', express.static(path.join(__dirname, 'temp-uploads')));
 
