aa82ea3c8016da70edb302a8b011f68f59aca908
Anandhu-Manoj <manojanandhu74@gmail.com>
2025-05-14 18:20:21 +0530

    deploy

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const modelRoutes = require('./Routes/route');
 const path=require('path')
 
 const app = express();
-app.use(cors());
+app.use(cors({origin: 'https://glb-viewer-ffn8c11g8-anandhu-manojs-projects-2d7d8f6e.vercel.app'}));
 app.use(express.json());
 app.use('/models', express.static(path.join(__dirname, 'temp-uploads')));
 
